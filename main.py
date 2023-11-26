0cf1521976e2465e4b1832014fc7c635e981c7ac
Wessel E. <wesselendtz1@gmail.com>
2023-11-26 12:55:10 +0100

    basic settings for main.py and for setup.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
+import os
+
 from cli.prompts import ask_for_name, ask_for_age
-from decouple import Config, Csv
-config = Config()
-
-DEBUG = config.getboolean("DEBUG")
-SECRET_KEY = config.get("SECRET_KEY")
-DATABASE_URL = config.get("DATABASE_URL")
-
 
 
 def main():
-    print("Welcome to My Project!")
-
-    name = ask_for_name()
-    age = ask_for_age()
-
-    print(f"Hello, {name}! You are {age} years old.")
+    if os.environ.get("AUTO_RUN") == "True":
+        pass
+    else:
+        print("Welcome to My Project!")
+        name = ask_for_name()
+        age = ask_for_age()
+        print(f"Hello, {name}! You are {age} years old.")
 
 
 if __name__ == "__main__":
